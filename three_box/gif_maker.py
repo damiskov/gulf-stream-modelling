70ab5471db3422b30c80255b8008d45013bdebaa
David Miles-Skov <davidmilesskov@protonmail.com>
2023-11-08 11:10:35 +0100

    added reduced three box-model + gifs

--- three_box/gif_maker.py
+++ three_box/gif_maker.py
@@ -2,8 +2,8 @@ from PIL import Image
 
 # List of image file names in the order you want them in the GIF
 fp = "gif1/"
-psis = ["01","02", "03","04", "05","06","07", "08", "09", "1", "2", "10"]
-fnames = [fp+"psi_" + psi + ".png" for psi in psis]
+k_d = [10000, 5000, 1000, 500, 100, 50, 10, 5, 1, 0.5, 0.1, 0.01, 0.001, 0.0001]
+fnames = [fp+f"2D_kd_{i}.png" for i in k_d]
 
 # Create a list to store the image objects
 images = []
@@ -14,4 +14,4 @@ for filename in fnames:
     images.append(img)
 
 # Save the GIF
-images[0].save("output.gif", save_all=True, append_images=images[1:], duration=100, loop=0)
+images[0].save("2D_k_d_ratio.gif", save_all=True, append_images=images[1:], duration=100, loop=0)
