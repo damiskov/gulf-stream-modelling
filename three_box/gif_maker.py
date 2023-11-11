68864a50593a239f6a282105a4aa222c9fca93a7
David Miles-Skov <davidmilesskov@protonmail.com>
2023-11-11 09:43:06 +0100

    many changes

--- three_box/gif_maker.py
+++ three_box/gif_maker.py
@@ -2,8 +2,8 @@ from PIL import Image
 
 # List of image file names in the order you want them in the GIF
 fp = "gif1/"
-k_d = [10000, 5000, 1000, 500, 100, 50, 10, 5, 1, 0.5, 0.1, 0.01, 0.001, 0.0001]
-fnames = [fp+f"2D_kd_{i}.png" for i in k_d]
+gammas = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, .7, 0.8, 0.9, 1]
+fnames = [fp+f"gamma_2D_{i}.png" for i in gammas]
 
 # Create a list to store the image objects
 images = []
@@ -14,4 +14,4 @@ for filename in fnames:
     images.append(img)
 
 # Save the GIF
-images[0].save("2D_k_d_ratio.gif", save_all=True, append_images=images[1:], duration=100, loop=0)
+images[0].save("gamma_2D.gif", save_all=True, append_images=images[1:], duration=100, loop=0)
